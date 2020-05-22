000d97afe9f395f10401e3e60fbd5e88d83fd2be
Desmond Wareham <desmond.wareham@gmail.com>
2020-05-22 14:03:33 -0400

    deployed and connected to db

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://blooming-dawn-02673.herokuapp.com/',
   TOKEN_KEY: 'process.env.REACT_APP_TOKEN_KEY',
 }
