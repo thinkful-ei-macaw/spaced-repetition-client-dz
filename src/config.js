04356479971cbebae6fc85eceeba8cea2aa09f00
Desmond Wareham <desmond.wareham@gmail.com>
2020-06-18 14:14:18 -0400

    updated env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://blooming-dawn-02673.herokuapp.com/',
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
   TOKEN_KEY: 'process.env.REACT_APP_TOKEN_KEY',
 }
