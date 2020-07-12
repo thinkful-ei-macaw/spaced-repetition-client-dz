85257e7fe137202407ce6cb89b1c652da4982cfb
Desmond Wareham <desmond.wareham@gmail.com>
2020-07-12 17:27:28 -0400

    deploying

--- src/config.js
+++ src/config.js
@@ -2,3 +2,10 @@ export default {
   API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
   TOKEN_KEY: 'process.env.REACT_APP_TOKEN_KEY',
 }
+// {
+//   API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://blooming-dawn-02673.herokuapp.com/api',
+//   TOKEN_KEY: 'process.env.REACT_APP_TOKEN_KEY',
+// }
+// export default{
+//   API_ENDPOINT:process.env.REACT_APP_API_ENDPOINT || 'https://gentle-atoll-30518.herokuapp.com',
+// }
